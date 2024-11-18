996c8afb826aead3561a9c16e37f612e1cd1f6a9
Swapnil Gamre <swapgmre@gmail.com>
2024-11-19 00:11:29 +0530

    created login and logut api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ const authRouter = express.Router();
 const User = require("../models/user");
 const { validateSignUpData } = require("../utils/validation");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 authRouter.post("/signup", async (req, res) => {
   try {
@@ -21,9 +22,40 @@ authRouter.post("/signup", async (req, res) => {
   }
 })
 
-authRouter.post("/login", (req, res) => {
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials!");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
+    if (isPasswordValid) {
+      const token = await jwt.sign({ _id: user._id }, "SwiftRide@$18112024", { expiresIn: "7d" });
+      res.cookie("token", token, { expires: new Date(Date.now() + 8 * 3600000) });
+      res.send("Login Successfull")
+    } else {
+      throw new Error("Invalid Credentials!")
+    }
+  } catch (err) {
+    res.send(`Error : ${err.message}`);
+  }
 });
+
+authRouter.post("/logout", async (req, res) => {
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now())
+    })
+    res.send("Logout Successfull")
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: `Error: ${err.message}` });
+  }
+
+})
+
 
 
 module.exports = authRouter;
